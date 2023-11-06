226eca97e2c908d20e493e0b068a89a05154d5dd
elamathidev <146702031+elamathidev@users.noreply.github.com>
2023-11-06 11:29:29 +0530

    Add files via upload
    Decreasing Triangle pattern

---
+++ StarDecreTriangle.java
@@ -0,0 +1,16 @@
+package assignment2.java;
+
+public class StarDecreTriangle {
+    public static void main(String[] args){
+        int row=7;
+        for(int i=0;i<row-1;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<row-i-1;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
