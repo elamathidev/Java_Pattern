6bec0dee314c3804e78007fea18d647b6f11083f
elamathidev <146702031+elamathidev@users.noreply.github.com>
2023-11-06 11:27:59 +0530

    Add files via upload
    Diamond pattern

---
+++ StarDiamond.java
@@ -0,0 +1,26 @@
+package assignment2.java;
+
+public class StarDiamond {
+    public static void main (String[] args){
+        int row = 7;
+        for(int i=0;i<row;i++) {
+            for (int j = 0; j <row - i-1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for(int i=0;i<row-1;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<row-i-1;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
